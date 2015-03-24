e3eb1dbc6eabf03b6166e13cb62ab5e4bf52c479
James <yuanyu.chang@coretronic.com>
2015-03-24 16:00:10 +0800

    Initial Commit

---
+++ TabPassValue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabPassValue
+//
+//  Created by James on 2015/3/24.
+//  Copyright (c) 2015年 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TabPassValue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabPassValue
+//
+//  Created by James on 2015/3/24.
+//  Copyright (c) 2015年 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
